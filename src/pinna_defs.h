0a4d4c3241e6a9baac944f3db923df066aa26505
shuoshuo.qin <shuoshuo.qin@tendcloud.com>
2016-10-22 16:21:36 +0800

    first commit.

---
+++ src/pinna_defs.h
@@ -0,0 +1,17 @@
+//
+// Created by QINSIMON on 2016/10/22.
+//
+
+#ifndef PINNA_PINNA_DEFS_H
+#define PINNA_PINNA_DEFS_H
+
+#define KEY_LENGTH 32
+//#define VAL_LENGTH 32
+#define LEVEL_DEPTH 4
+
+typedef struct antKeyVal {
+    char key[KEY_LENGTH];
+    unsigned long long val;
+};
+
+#endif //PINNA_PINNA_DEFS_H
